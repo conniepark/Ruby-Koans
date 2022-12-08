7374df89afa5f55321ba599f23cd4766bdc0361d
conniepark <119345013+conniepark@users.noreply.github.com>
2022-12-08 09:20:33 -0800

    Update about_iteration.rb

--- about_iteration.rb
+++ about_iteration.rb
@@ -95,10 +95,14 @@ class AboutIteration < Neo::Koan
     result = [2, 3, 4].inject(0) { |sum, item| sum + item }
     ##assert_equal __, result
     assert_equal 2+3+4, result
+    #[2, 3, 4].inject {|sum, number| sum + number} =>|2, 3| 2 + 3 => 5
+    #                                               =>|5, 4| 5 + 4 => 9
 
     result2 = [2, 3, 4].inject(1) { |product, item| product * item }
     ##assert_equal __, result2
     assert_equal 2*3*4, result2
+    #[2, 3, 4].inject {|sum, number| sum * number} =>|2, 3| 2 * 3 => 6
+    #                                               =>|6, 4| 6 * 4 => 24
 
     # Extra Credit:
     # Describe in your own words what inject does.
